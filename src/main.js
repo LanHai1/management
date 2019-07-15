bbd65d7344143dea6c806ca90cc5784ef34e4e0a
lanhai <1444133004@qq.com>
2019-07-15 13:59:52 +0800

    登陆模块组件完成

--- src/main.js
+++ src/main.js
@@ -1,8 +1,45 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
-Vue.config.productionTip = false
+// 导入路由模块
+import VueRouter from "vue-router";
+Vue.use(VueRouter);
+
+// 导入饿了么模块
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+Vue.use(ElementUI);
+
+// 配置axios
+import axios from "axios";
+// 配置基地址
+axios.defaults.baseURL = "http://localhost:8888/api/private/v1/";
+Vue.prototype.$axios = axios;
+
+// 登陆组件
+import login from "./views/login/login.vue";
+const routes = [
+  {
+    path: "/",
+    // 重定向
+    redirect: "/login"
+  },
+  {
+    path: "/login",
+    component: login
+  }
+];
+
+const router = new VueRouter({
+  routes
+});
+
+Vue.config.productionTip = false;
+
+// 全局样式
+import "./assets/css/reset.css";
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+  router
+}).$mount("#app");
---
+++ src/views/login/login.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="bg">
+    <div class="form">
+      <h1 class="title">用户登陆</h1>
+      <el-form
+        :model="ruleForm"
+        status-icon
+        ref="ruleForm"
+        label-width="100px"
+        class="demo-ruleForm"
+        label-position="top"
+      >
+        <el-form-item label="用户名" prop="user" :rules="[{ required: true, message: '姓名不能为空'}]">
+          <el-input type="text" v-model.trim="ruleForm.user" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="pass" :rules="[{ required: true, message: '密码不能为空'}]">
+          <el-input type="password" v-model.trim="ruleForm.pass" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="submitForm('ruleForm')" class="submit">提交</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "login",
+  data() {
+    return {
+      ruleForm: {
+        user: "",
+        pass: ""
+      }
+    };
+  },
+  methods: {
+    // 错误提示
+    errPrompt(msg) {
+      this.$notify.error({
+        title: "错误",
+        message: msg,
+        duration: 2000
+      });
+    },
+    // 提交数据
+    submitForm() {
+      const { user, pass } = this.ruleForm;
+      // 为空不发请求
+      if (!user || !pass) return this.errPrompt("用户名或密码不能为空");
+      this.$axios
+        .post("login", {
+          username: user,
+          password: pass
+        })
+        .then(res => {
+          if (res.data.meta.status === 400) {
+            // 重置用户输入
+            this.ruleForm.user = "";
+            this.ruleForm.pass = "";
+            return this.errPrompt(res.data.meta.msg);
+          }
+          if (res.data.meta.status === 200) {
+            return this.$message({
+              message: res.data.meta.msg,
+              type: "success"
+            });
+          }
+        })
+        .catch(err => {
+          console.error(err);
+        });
+    }
+  }
+};
+</script>
+
+<style scoped lang="less">
+.bg {
+  background-color: #263141;
+  position: fixed;
+  left: 0;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  .title {
+    font-size: 24px;
+    font-weight: 600;
+    padding: 20px 0 35px;
+  }
+  .form {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    padding: 40px;
+    width: 500px;
+    border-radius: 5px;
+    background-color: #fff;
+  }
+  .submit {
+    width: 100%;
+  }
+}
+</style>
