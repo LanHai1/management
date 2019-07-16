a188da58a78e828c63ba6ccabd4d1c443786acb7
lanhai <1444133004@qq.com>
2019-07-16 20:50:53 +0800

    axios拦截器

--- src/api/http.js
+++ src/api/http.js
@@ -12,6 +12,33 @@ const http = axios.create({
   // headers: { "X-Custom-Header": "foobar" }
 });
 
+// axios 拦截器
+// 请求拦截器
+http.interceptors.request.use(
+  function(config) {
+    // Do something before request is sent
+    // 所有请求 携带token
+    config.headers.Authorization = window.localStorage.getItem("token");
+    return config;
+  },
+  function(error) {
+    // Do something with request error
+    return Promise.reject(error);
+  }
+);
+
+// 响应拦截器
+http.interceptors.response.use(
+  function(response) {
+    // Do something with response data
+    return response;
+  },
+  function(error) {
+    // Do something with response error
+    return Promise.reject(error);
+  }
+);
+
 // 登陆
 export const login = ({ username, password }) => {
   return http.post("login", {
